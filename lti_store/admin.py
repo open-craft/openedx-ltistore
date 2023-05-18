9d1b22b6faa2d773ea72a57146c51f90961a2c1e
César Montilla <cesar.montilla@edunext.co>
2023-05-18 05:56:43 +0000

    feat: LTI 1.3 reusable configuration
    Co-authored-by: Squirrel18 <daniel.quiroga@edunext.co>
    Co-authored-by: alexjmpb <alexander.mendoza@edunext.co>
    Co-authored-by: anfbermudezme <andres.bermudez@edunext.co>
    Co-authored-by: sergiovalero20 <sergio.valero@edunext.co>

--- lti_store/admin.py
+++ lti_store/admin.py
@@ -8,6 +8,7 @@ class LtiConfigurationAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "version", "filter_key")
     list_filter = ("version",)
     prepopulated_fields = {"slug": ("name",)}
+    readonly_fields = ("lti_1p3_public_jwk",)
 
     def filter_key(self, obj):
         return f"{App.name}:{obj.slug}"
