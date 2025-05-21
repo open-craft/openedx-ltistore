950b300df8cff35de9b9f26256f66305834b7f5a
Agrendalath <piotr@surowiec.it>
2025-05-21 19:37:29 +0200

    feat: unpin Django, add CI, update requirements

--- Makefile
+++ Makefile
@@ -3,3 +3,30 @@ lint:
 
 test:
 	pytest --cov-report term-missing lti_store
+
+
+piptools: ## install uv
+	if command -v uv >/dev/null 2>&1; then \
+		uv pip install -r requirements/pip.txt; \
+	else \
+		pip install -r requirements/pip.txt; \
+	fi
+
+requirements: piptools ## install development environment requirements
+	uv venv --allow-existing
+	uv pip sync -q requirements/dev.txt requirements/private.*
+
+# Define PIP_COMPILE_OPTS=-v to get more information during make upgrade.
+PIP_COMPILE = uv pip compile --upgrade $(PIP_COMPILE_OPTS)
+
+upgrade: export CUSTOM_COMPILE_COMMAND=make upgrade
+upgrade: piptools $(COMMON_CONSTRAINTS_TXT) ## update the requirements/*.txt files with the latest packages satisfying requirements/*.in
+	uv venv --allow-existing
+	# Make sure to compile files after any other files they include!
+	$(PIP_COMPILE) -o requirements/pip.txt requirements/pip.in
+	uv pip install -qr requirements/pip.txt
+	$(PIP_COMPILE) -o requirements/base.txt requirements/base.in
+	$(PIP_COMPILE) -o requirements/dev.txt requirements/dev.in
+	$(PIP_COMPILE) -o requirements/test.txt requirements/dev.in
+	# Let tox control the Django version for tests
+	sed -i '/^[dD]jango==/d' requirements/test.txt
--- setup.py
+++ setup.py
@@ -2,16 +2,15 @@
 """
 Package metadata for openedx_ltistore
 """
-from gettext import find
-from importlib.metadata import entry_points
 import os
 import re
 import sys
+from pathlib import Path
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 
-def get_version(*file_paths):
+def get_version(file_path: Path) -> str:
     """
     Extract the version string from the file.
 
@@ -19,43 +18,111 @@ def get_version(*file_paths):
      - file_paths: relative path fragments to file with
                    version string
     """
-    filename = os.path.join(os.path.dirname(__file__), *file_paths)
-    version_file = open(filename).read()
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    filename = Path(__file__).parent / file_path
+    with Path(filename).open(encoding="utf8") as f:
+        version_file = f.read()
+        version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.MULTILINE)
     if version_match:
         return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
+    raise RuntimeError('Unable to find version string.')  # noqa: EM101
 
 
-def load_requirements(*requirements_paths):
+def load_requirements(*requirements_paths: Path) -> list[str]:  # noqa: C901
     """
     Load all requirements from the specified requirements files.
 
-    Returns:
-        list: Requirements file relative path strings
+    Requirements will include any constraints from files specified
+    with -c in the requirements files.
+    Returns a list of requirement strings.
     """
-    requirements = set()
+    # e.g. {"django": "Django", "confluent-kafka": "confluent_kafka[avro]"}
+    by_canonical_name = {}
+
+    def check_name_consistent(package: str) -> None:
+        """
+        Raise exception if package is named different ways.
+
+        This ensures that packages are named consistently so we can match
+        constraints to packages. It also ensures that if we require a package
+        with extras we don't constrain it without mentioning the extras (since
+        that too would interfere with matching constraints.)
+        """
+        canonical = package.lower().replace('_', '-').split('[')[0]
+        seen_spelling = by_canonical_name.get(canonical)
+        if seen_spelling is None:
+            by_canonical_name[canonical] = package
+        elif seen_spelling != package:
+            raise Exception(  # noqa: TRY002
+                f'Encountered both "{seen_spelling}" and "{package}" in requirements '  # noqa: EM102
+                'and constraints files; please use just one or the other.',
+            )
+
+    requirements = {}
+    constraint_files = set()
+
+    # groups "pkg<=x.y.z,..." into ("pkg", "<=x.y.z,...")
+    re_package_name_base_chars = r"a-zA-Z0-9\-_."  # chars allowed in base package name
+    # Two groups: name[maybe,extras], and optionally a constraint
+    requirement_line_regex = re.compile(
+        fr"([{re_package_name_base_chars}]+(?:\[[{re_package_name_base_chars},\s]+])?)([<>=][^#\s]+)?",
+    )
+
+    def add_version_constraint_or_raise(
+        current_line: str,
+        current_requirements: dict[str, str],
+        add_if_not_present: bool,  # noqa: FBT001
+    ):
+        regex_match = requirement_line_regex.match(current_line)
+        if regex_match:
+            package = regex_match.group(1)
+            version_constraints = regex_match.group(2)
+            check_name_consistent(package)
+            existing_version_constraints = current_requirements.get(package)
+            # It's fine to add constraints to an unconstrained package,
+            # but raise an error if there are already constraints in place.
+            if existing_version_constraints and existing_version_constraints != version_constraints:
+                # noinspection PyExceptionInherit
+                raise BaseException(  # noqa: TRY002
+                    f'Multiple constraint definitions found for {package}:'  # noqa: EM102
+                    f' "{existing_version_constraints}" and "{version_constraints}".'
+                    f'Combine constraints into one location with {package}'
+                    f'{existing_version_constraints},{version_constraints}.',
+                )
+            if add_if_not_present or package in current_requirements:
+                current_requirements[package] = version_constraints
+
+    # Read requirements from .in files and store the path to any
+    # constraint files that are pulled in.
     for path in requirements_paths:
-        requirements.update(
-            line.split("#")[0].strip()
-            for line in open(path).readlines()
-            if is_requirement(line.strip())
-        )
-    return list(requirements)
+        with path.open() as reqs:
+            for line in reqs:
+                if is_requirement(line):
+                    add_version_constraint_or_raise(line, requirements, add_if_not_present=True)
+                if line and line.startswith('-c') and not line.startswith('-c http'):
+                    constraint_files.add(path.parent / line.split('#')[0].replace('-c', '').strip())
+
+    # process constraint files: add constraints to existing requirements
+    for constraint_file in constraint_files:
+        with constraint_file.open() as reader:
+            for line in reader:
+                if is_requirement(line):
+                    add_version_constraint_or_raise(line, requirements, add_if_not_present=False)
+
+    # process back into list of pkg><=constraints strings
+    return [f'{pkg}{version or ""}' for (pkg, version) in sorted(requirements.items())]
 
 
-def is_requirement(line):
+def is_requirement(line: str) -> bool:
     """
     Return True if the requirement line is a package requirement.
 
     Returns:
-        bool: True if the line is not blank, a comment, a URL, or
-              an included file
+        bool: True if the line is not blank, a comment, a URL, or an included file.
     """
-    return line and not line.startswith(("-r", "#", "-e", "git+", "-c"))
+    return bool(line and line.strip() and not line.startswith(("-r", "#", "-e", "git+", "-c")))
 
 
-VERSION = get_version("lti_store", "__init__.py")
+VERSION = get_version(Path('lti_store/__init__.py'))
 
 if sys.argv[-1] == "tag":
     print("Tagging the version on github:")
@@ -63,21 +130,26 @@ if sys.argv[-1] == "tag":
     os.system("git push --tags")
     sys.exit()
 
-README = open(os.path.join(os.path.dirname(__file__), "README.md")).read()
-CHANGELOG = open(os.path.join(os.path.dirname(__file__), "CHANGELOG.md")).read()
+README = (Path(__file__).parent / 'README.md').open(encoding="utf8").read()
+CHANGELOG = (Path(__file__).parent / 'CHANGELOG.md').open(encoding="utf8").read()
 
 setup(
     name="openedx-ltistore",
     version=VERSION,
     description="""An app for storing LTI provider configurations centrally.""",
     long_description=README + "\n\n" + CHANGELOG,
+    long_description_content_type='text/markdown',
     author="edX",
     author_email="oscm@edx.org",
     url="https://github.com/openedx/openedx-ltistore",
-    packages=find_packages(),
+    packages=find_packages(
+        include=["lti_store"],
+        exclude=["*tests"],
+    ),
     include_package_data=True,
-    install_requires=["django<4.0"],
-    python_requires=">=3.8",
+    install_requires=load_requirements(Path('requirements/base.in')),
+    options={'bdist_wheel': {'universal': True}},
+    python_requires=">=3.11",
     license="AGPL 3.0",
     zip_safe=False,
     keywords="Python edx",
@@ -87,7 +159,11 @@ setup(
         "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Framework :: Django",
+        "Framework :: Django :: 4.2",
+        "Framework :: Django :: 5.2",
     ],
     entry_points={
         "lms.djangoapp": [
@@ -96,5 +172,5 @@ setup(
         "cms.djangoapp": [
             "lti_store = lti_store.apps:LtiStoreConfig",
         ],
-    }
+    },
 )
